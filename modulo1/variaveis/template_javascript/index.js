84d0b8e03f35aeee1ee09a0f57f87586c24d619d
Mariotin <gmariotin@gmail.com>
2022-05-09 14:35:48 -0300

    Exercicio javascript

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,46 @@
+/* Respostas dos Exercícios de interpretação dia 09 de Maio
+
+R 1: 10 5
+R 2: 10 10 10
+R 3: Variavel p = horasTrabalhadasPorDia
+     Variavel t = salarioPorDia
+
+*/
+
+// let nome;
+// let idade;
+// console.log (typeof nome, typeof idade);
+// // ambas variaveis são undefined, pois não tem um valor atribuido a elas.
+
+// nome = prompt ("Qual é o seu nome?");
+// idade = prompt ("Qual é a sua idade?")
+// console.log (nome, idade);
+// console.log (typeof nome, typeof idade);
+// // ambas viraram String
+
+// console.log ("Olá", nome, "você tem", idade, "anos");
+
+// let P1 = prompt("Você está de tenis?");
+// console.log ("Você está de tenis?", P1);
+// let P2 = prompt("Você é careca?");
+// console.log ("Você é careca?", P2);
+// let P3 = prompt("Você está alegre?");
+// console.log ("Você está alegre?", P3);
+
+// let a = 10
+// let b = 25
+
+// let c = 25
+// b=a
+// a=c
+
+// console.log("O novo valor de a é", a) // O novo valor de a é 25
+// console.log("O novo valor de b é", b) // O novo valor de b é 10
+
+let numero1 = Number(prompt ("Digite um numero"));
+let numero2 = Number(prompt ("Digite outro numero"));
+console.log ("O Primeiro número digitado foi:", numero1 );
+console.log ("O Segundo número digitado foi:", numero2 );
+
+console.log ("O primeiro número somado ao segundo número resulta em", numero1+numero2);
+console.log ("O primeiro número multiplicado  ao segundo número resulta em", numero1*numero2);
