f2bc62d631dcc3996d90d82ac2c9ed885d3eb2ad
Mariotin <gmariotin@gmail.com>
2022-05-11 17:00:40 -0300

    Exercicio strings-arrays

---
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,59 @@
+/* Exercícios de interpretação de código
+1 - a. indefinido
+    b. null
+    c. 11
+    d. 3
+    e. 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    f. 9
+
+2 - SUBI NUM ÔNIBUS EM MIRROCOS 27
+*/
+
+// Exercícios de escrita de código
+// Exercício 1
+
+// const nomeDoUsuario = prompt("Qual é o seu nome?");
+// const emailDoUsuario = prompt("Qual é seu email?");
+// console.log("O e-mail " + emailDoUsuario + " foi cadastrado com sucesso. Seja bem-vinda(o), " + nomeDoUsuario + "!");
+
+//Exercício 2
+
+// const comidasFavoritas = ["Sushi", "Lasanha", "Pizza", "Churrasco", "Hamburguer"];
+// console.log(comidasFavoritas);
+
+// console.log(`Essas são as minhas comidas preferidas: \n${comidasFavoritas[0]} 
+// ${comidasFavoritas[1]} 
+// ${comidasFavoritas[2]}
+// ${comidasFavoritas[3]}
+// ${comidasFavoritas[4]}`);
+
+// const comidaUsuario = prompt("Qual sua comida preferida?")
+// comidasFavoritas[1] = comidaUsuario
+// console.log(comidasFavoritas)
+
+//Exercício 3
+
+// const listaDeTarefas = []
+// const tarefa1 = prompt("Digite a primeira tarefa");
+// const tarefa2 = prompt("Digite a segunda tarefa");
+// const tarefa3 = prompt("Digite a terceira tarefa");
+// listaDeTarefas.push (tarefa1, tarefa2, tarefa3);
+// console.log(listaDeTarefas);
+
+// const tarefaRealizada = Number (prompt("Qual tarefa já foi realizada?"));
+// listaDeTarefas.splice (tarefaRealizada, 1);
+// console.log (listaDeTarefas)
+
+//Desafio 1
+
+// const frase = prompt ("Digite uma frase");
+// const array = frase.split(" ");
+// console.log(array);
+
+
+//Desafio 2
+//  const frutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"];
+//  let index = frutas.indexOf("Abacaxi");
+// console.log (index, frutas.length);
+
+
