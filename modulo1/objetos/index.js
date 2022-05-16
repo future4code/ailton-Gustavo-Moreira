4d92f215cfbd659203885247d4b8e0c4263e813d
Mariotin <gmariotin@gmail.com>
2022-05-16 19:58:49 -0300

    Atualizado

--- modulo1/objetos/index.js
+++ modulo1/objetos/index.js
@@ -13,113 +13,114 @@
 // Exercícios de escrita de código
 // Exercício 1 
 
-const pessoa = {
-    nome: "Gustavo",
-    apelido: ["Mario", "Mariots", "Mariotinho"],
-}
+// const pessoa = {
+//     nome: "Gustavo",
+//     apelido: ["Mario", "Mariots", "Mariotinho"],
+// }
 
-function imprime (obj){
-    const pessoa1 = obj;
-    const novaPessoa = {
-        ...obj,
-        apelido: ["Mariotis", "Mariotones", "Maroto"]
-        }
-    console.log (`Eu sou ${pessoa1.nome}, mas pode me chamar de: ${pessoa1.apelido[0]}, ${pessoa1.apelido[1]} ou ${pessoa1.apelido[2]}`); 
-    console.log (`Eu sou ${novaPessoa.nome}, mas pode me chamar de: ${novaPessoa.apelido[0]}, ${novaPessoa.apelido[1]} ou ${novaPessoa.apelido[2]}`)
-    }
-    imprime (pessoa)
+// function imprime (obj){
+//     const pessoa1 = obj;
+//     const novaPessoa = {
+//         ...obj,
+//         apelido: ["Mariotis", "Mariotones", "Maroto"]
+//         }
+//     console.log (`Eu sou ${pessoa1.nome}, mas pode me chamar de: ${pessoa1.apelido[0]}, ${pessoa1.apelido[1]} ou ${pessoa1.apelido[2]}`); 
+//     console.log (`Eu sou ${novaPessoa.nome}, mas pode me chamar de: ${novaPessoa.apelido[0]}, ${novaPessoa.apelido[1]} ou ${novaPessoa.apelido[2]}`)
+//     }
+//     imprime (pessoa)
   
-// Exercício 2
+// // Exercício 2
 
-const objeto1 = {
-  nome: "João",
-  idade: 20,
-  profissão: "Estudante",
-}
+// const objeto1 = {
+//   nome: "João",
+//   idade: 20,
+//   profissão: "Estudante",
+// }
 
-const objeto2 = {
-  nome: "Antonio",
-  idade: 40,
-  profissão: "Professor",
-}
+// const objeto2 = {
+//   nome: "Antonio",
+//   idade: 40,
+//   profissão: "Professor",
+// }
 
-function info (Obj) {
-  const array = Obj;
+// function info (Obj) {
+//   const array = Obj;
 
-  console.log (`["${array.nome}", ${array.nome.length}, ${array.idade}, "${array.profissão}", ${array.profissão.length}]`);
-  return array
-}
-info (objeto1)
-info (objeto2)
+//   console.log (`["${array.nome}", ${array.nome.length}, ${array.idade}, "${array.profissão}", ${array.profissão.length}]`);
+//   return array
+// }
+// info (objeto1)
+// info (objeto2)
 
-// Exercício 3
-const carrinho = [];
+// // Exercício 3
+// const carrinho = [];
 
-const fruta1 = {
-  nome: "Banana",
-  dispo: true,
-}
-const fruta2 ={
-  nome: "Morango",
-  dispo: true,
-}
-const fruta3 ={
-  nome: "Maça",
-  dispo: true,
-}
+// const fruta1 = {
+//   nome: "Banana",
+//   dispo: true,
+// }
+// const fruta2 ={
+//   nome: "Morango",
+//   dispo: true,
+// }
+// const fruta3 ={
+//   nome: "Maça",
+//   dispo: true,
+// }
 
-function adicionar (fruta) {
-  carrinho.push(fruta);
-  return carrinho
-}
+// function adicionar (fruta) {
+//   carrinho.push(fruta);
+//   return carrinho
+// }
 
-adicionar (fruta1)
-adicionar (fruta2)
-adicionar (fruta3)
-console.log(carrinho)
+// adicionar (fruta1)
+// adicionar (fruta2)
+// adicionar (fruta3)
+// console.log(carrinho)
 
-// Desafios
-//Desafio 1
-const nome = prompt ("Digite seu nome");
-const idade = prompt ("Digite sua idade");
-const prof = prompt ("Digite sua profissão");
+// // Desafios
+// //Desafio 1
 
-function dados (nome, idade, prof){
-  const dadoPessoal = {
 
-  }
-  dadoPessoal.nome = nome
-  dadoPessoal.idade = idade
-  dadoPessoal.profissão = prof
-  console.log (dadoPessoal)
+// function dados (){
+// const nome = prompt ("Digite seu nome");
+// const idade = prompt ("Digite sua idade");
+// const prof = prompt ("Digite sua profissão");
+//   const dadoPessoal = {
+
+//   }
+//   dadoPessoal.nome = nome
+//   dadoPessoal.idade = idade
+//   dadoPessoal.profissão = prof
+//   console.log (dadoPessoal)
   
-}
-dados (nome, idade, prof)
+// }
+// dados ()
 
-// Desafio 2
+// // Desafio 2
 
-const filme1 ={
-  ano_lancamento: 2022,
-  nome: "A volta dos que não foram"
-}
-const filme2 ={
-  ano_lancamento: 2022,
-  nome: "A ida dos que nao partiram"
-}
-function infoFilme (filme1, filme2){
-  const resultadoAntes = filme1.ano_lancamento < filme2.ano_lancamento
-  const resultadoIgual = filme1.ano_lancamento === filme2.ano_lancamento
+// const filme1 ={
+//   ano_lancamento: 2022,
+//   nome: "A volta dos que não foram"
+// }
+// const filme2 ={
+//   ano_lancamento: 2022,
+//   nome: "A ida dos que nao partiram"
+// }
+// function infoFilme (filme1, filme2){
+//   const resultadoAntes = filme1.ano_lancamento < filme2.ano_lancamento
+//   const resultadoIgual = filme1.ano_lancamento === filme2.ano_lancamento
 
-  console.log (`O ${filme1.nome} foi lançado antes do ${filme2.nome}?`, resultadoAntes)
-  console.log (`O ${filme1.nome} foi lançado no mesmo ano do ${filme2.nome}?`, resultadoIgual)  
-  return infoFilme
-}
-infoFilme(filme1, filme2)
+//   console.log (`O ${filme1.nome} foi lançado antes do ${filme2.nome}?`, resultadoAntes)
+//   console.log (`O ${filme1.nome} foi lançado no mesmo ano do ${filme2.nome}?`, resultadoIgual)  
+//   return infoFilme
+// }
+// infoFilme(filme1, filme2)
 
-// Desafio 3
+// // Desafio 3
 
-function controleEstoque (fruta) {
-  fruta.dispo = !fruta.dispo
-}
-controleEstoque (fruta1)
-controleEstoque (fruta2)
+// function controleEstoque (fruta) {
+//   fruta.dispo = !fruta.dispo
+// }
+// controleEstoque (fruta1)
+// controleEstoque (fruta2)
