fb674c13caeff0b40ede412c24e627a5b2f698df
Mariotin <gmariotin@gmail.com>
2022-05-22 19:09:37 -0300

    atualizaçao

--- modulo1/projeto-blackjack/Blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/Blackjack/nossoJogo.js
@@ -10,3 +10,33 @@
  * 
  * 
  */
+
+// console.log ("Boas vindas ao jogo de Blackjack!")
+
+// while (confirm("Quer iniciar uma nova rodada?")) {
+     
+// const carta = comprarCarta();
+
+
+// let cartaP1 = comprarCarta();
+// let cartaP2 = comprarCarta();
+// let maoPlayer = cartaP1.valor + cartaP2.valor;
+
+// let cartaM1 = comprarCarta();
+// let cartaM2 = comprarCarta();
+// let maoPC = cartaM1.valor + cartaM2.valor;
+
+
+// console.log('Suas cartas são: ' + cartaP1.texto + '  ' + cartaP2.texto + ' - pontuação ' + maoPlayer);
+// console.log('As cartas do oponente são: ' + cartaM1.texto + '  ' + cartaM2.texto + ' - pontuação ' + maoPC);
+
+// if (maoPlayer > maoPC) {
+//    console.log("Voce venceu")
+// } else if (maoPlayer < maoPC){
+//    console.log("Voce perdeu")
+// } else {
+//    console.log("Empatou")
+// }
+// } 
+//    console.log("O jogo acabou!")
+
