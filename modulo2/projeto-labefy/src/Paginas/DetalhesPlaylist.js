8b98237e82fd52f438f451c1717c0329cf3b9d6d
Mariotin <gmariotin@gmail.com>
2022-06-25 22:16:18 -0300

    atualiza

--- modulo2/projeto-labefy/src/Paginas/DetalhesPlaylist.js
+++ modulo2/projeto-labefy/src/Paginas/DetalhesPlaylist.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
 
 export default class DetalhesPlaylist extends Component {
+
+  state = {
+    detalhe: {},
+    nomeMusica:"",
+  }
+
+  getPlaylistTracks = () => {
+    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${this.props.playlistId}/tracks`
+    const header = {
+      headers: {
+        Authorization: "gustavo-moreira-ailton"
+      }
+    }
+    axios
+      .get(url, header)
+      .then((res) => {
+        console.log (res);
+      })
+      .catch((erro) => console.log(erro.response))
+  }
   render() {
     return (
-      <div>DetalhesPlaylist</div>
+      <div>
+        <p>DetalhesPlaylist</p>
+        <button onClick={this.props.trocarTela}>
+          Voltar para lista de Playlist
+        </button>
+      </div>
     )
   }
 }
--- modulo2/projeto-labefy/src/Paginas/VisualizaPlaylist.js
+++ modulo2/projeto-labefy/src/Paginas/VisualizaPlaylist.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
+import DetalhesPlaylist from './DetalhesPlaylist'
 
 export default class VisualizaPlaylist extends Component {
   state = {
     playList: [],
+    paginaAtual: "Visualiza",
+    name: "",
+    playlistId: "",
   }
 
   componentDidMount() {
     this.getPlaylist();
   }
 
-  getPlaylist = () =>{
+  getPlaylist = () => {
     const url = "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists"
     const header = {
       headers: {
@@ -19,11 +23,11 @@ export default class VisualizaPlaylist extends Component {
       }
     }
     axios
-    .get(url, header)
-    .then((res) => {
-      this.setState({playList: res.data.result.list});
-    })
-    .catch((erro) => console.log (erro.response))
+      .get(url, header)
+      .then((res) => {
+        this.setState({ playList: res.data.result.list });
+      })
+      .catch((erro) => console.log(erro.response))
   }
 
   deletePlaylist = (id) => {
@@ -34,43 +38,77 @@ export default class VisualizaPlaylist extends Component {
       }
     }
     axios
-    .delete(url, header)
-    .then(() =>{
-      alert("Playlist apagada com sucesso!");
-      this.getPlaylist();
-    })
-    .catch((erro) =>{
-      alert("Erro ao deletar a Playlist. Tente novamente.")
-      console.log(erro)
-    })
+      .delete(url, header)
+      .then(() => {
+        alert("Playlist apagada com sucesso!");
+        this.getPlaylist();
+      })
+      .catch((erro) => {
+        alert("Erro ao deletar a Playlist. Tente novamente.")
+        console.log(erro)
+      })
   }
 
-  // searchPlaylist = () => {
-  //   const url = `https://us-centconsole.logral1-labenu-apis.cloudfunctions.net/labefy/playlists/search?name=string`
-  //   const header = {
-  //     headers: {
-  //       Authorization: "gustavo-moreira-ailton"
-  //     }
-  //   }
-  //   axios
-  // }
+  trocarTela = (id) => {
+    if (this.state.paginaAtual === "Visualiza") {
+      this.setState({ paginaAtual: "Detalhe", playlistId: id });
+    } else {
+      this.setState({ paginaAtual: "Visualiza", playlistId: "" });
+    }
+  }
+
+  inputName = (event) =>{
+    const newName = event.target.value
+    this.setState ({name: newName})
+  }
+
+  searchPlaylist = () => {
+    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/search?name=${this.state.name}`
+    const header = {
+      headers: {
+        Authorization: "gustavo-moreira-ailton"
+      }
+    }
+    axios
+      .get(url, header)
+      .then((res) => {
+        this.setState({playList: res.data.result.playlist})
+      })
+      .catch((erro) => console.log(erro.response))
+  }
 
   render() {
     console.log(this.state.playList)
     return (
       <div>
-        <h1>Essas são suas Playlists Criadas</h1>
-        {/* <h4>Procurar Playlist</h4>
-        <input
-        placeholder='Nome da Playlist'
-        ></input> */}
-        <section>
-          {this.state.playList.map((list) =>{
-              return <li key={list.id}> 
-                <span key={list.id}> {list.name} <button onClick={() => this.deletePlaylist(list.id)}>Deletar</button></span>
-                </li>
-          })}
-        </section>
+        {this.state.paginaAtual === "Visualiza" ? (
+          <div>
+            <h1>Essas são suas Playlists Criadas</h1>
+            <input
+              value={this.state.name}
+              placeholder='Procurar Playlist'
+              onChange={this.inputName}
+            ></input>
+            <button onClick={this.searchPlaylist}>Procurar</button>
+            <section>
+              {this.state.playList
+                .map((list) => {
+                  return <li key={list.id}>
+                    <span key={list.id}> {list.name} 
+                    <button onClick={( )=> this.trocarTela(list.id)}>Detalhes</button>
+                    <button onClick={() => this.deletePlaylist(list.id)}>Deletar</button>
+                    </span>
+                  </li>
+                })}
+
+            </section>
+          </div>
+        ) : (
+          <DetalhesPlaylist
+            playlistId={this.state.playlistId}
+            trocarTela={this.trocarTela}
+          />
+        )}
       </div>
     )
   }
