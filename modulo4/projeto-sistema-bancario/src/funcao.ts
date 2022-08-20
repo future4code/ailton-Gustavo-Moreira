db83860c38dd4e7381efd53ae9dca14ea3459cdc
Mariotin <gmariotin@gmail.com>
2022-08-20 03:58:08 -0300

    sexta

---
+++ modulo4/projeto-sistema-bancario/src/funcao.ts
@@ -0,0 +1,19 @@
+function idade(ano_aniversario: number, mes_aniversario: number, dia_aniversario: number) {
+    var d = new Date,
+        ano_atual = d.getFullYear(),
+        mes_atual = d.getMonth() + 1,
+        dia_atual = d.getDate(),
+
+        ano_aniversario: number = +ano_aniversario,
+        mes_aniversario: number = +mes_aniversario,
+        dia_aniversario: number = +dia_aniversario,
+
+        quantos_anos = ano_atual - ano_aniversario;
+
+    if (mes_atual < mes_aniversario || mes_atual == mes_aniversario && dia_atual < dia_aniversario) {
+        quantos_anos--;
+    }
+
+    return quantos_anos < 0 ? 0 : quantos_anos;
+}
+
